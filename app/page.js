11eb2c99228140dc57d8459675714d003870b3f5
nuvicteam-afk <nuvicteam@gmail.com>
2026-02-19 18:42:27 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -43,7 +43,7 @@ export default function Page() {
   <img
     src="/nuvi-logo.png"
     alt="NUVI Welding and Fabrication LLC Logo"
-    className="h-140 w-auto"
+    className="h-135 w-auto"
   />
   <a
     href="tel:4698455728"
